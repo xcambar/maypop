83179226d6da13e8f56846c929fe638eafc6f16f
Xavier Cambar <xcambar@gmail.com>
2013-07-09 01:51:00 -0600

    Passes 2 basic tests of API

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 "use strict";
 
-module.exports = function () {
+module.exports = function (fn) {
+  if(arguments.length === 0) {
+    throw new Error('You must pass a parameter to maypop');
+  }
+  return function () {
 
+  };
 };
